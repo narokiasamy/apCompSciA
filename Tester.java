adf689a3fb545df86ec5040f9402f8df22b27aca
narokiasamy <51964705+narokiasamy@users.noreply.github.com>
2019-11-04 23:17:21 -0500

    tester

---
+++ Tester.java
@@ -0,0 +1,17 @@
+public class Tester {
+  public static void main (String [] args) {
+  
+
+ for (int i=1; i<=9; i++)
+{
+   System.out.println();
+   for (int j=1; j<=i; j++)
+   {
+     System.out.print(j);
+   }
+}
+
+
+  
+  }  
+}
